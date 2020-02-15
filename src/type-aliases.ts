b490c7d7d66662d26fcf52fc3c630091a9f865c8
Yudai Yamamoto <udai11up@gmail.com>
2020-02-15 23:06:02 +0900

    type-aliases

---
+++ src/type-aliases.ts
@@ -0,0 +1,22 @@
+export {};
+
+type Mojiretsu = string;
+const fooString: string = 'Hello';
+const fooMojiretsu: Mojiretsu = 'Hello';
+
+const example1 = {
+  name: 'abc',
+  age: 43
+};
+
+type Profile = {
+  name: string;
+  age: number;
+}
+
+const example2: Profile = {
+  name: 'abc',
+  age: 43
+};
+
+type Profile2 = typeof example1;
